c61eb8c6ee59b847be0b4d0d375af8ff995b2151
zpcalan <1303359419@qq.com>
2026-04-17 16:24:12 +0800

    Print image of pickles

--- RT-X/parse_robo_data.py
+++ RT-X/parse_robo_data.py
@@ -1,20 +1,20 @@
 import pickle
 import io
 from PIL import Image
-with open('/data/k8s/zpc/spirit-v1.5/home_proj/RT-X/sample_000000087204.data.pickle', 'rb') as f:
+with open('/data/k8s/zpc/tiny-vla/RT-X/sample_berkeley_gnm_cory_hall.data.pickle', 'rb') as f:
     data = pickle.load(f)
 
-print(data.keys())
-for key, value in data['steps'][0].items():
-    print("=====key: ", key)
-    if key == 'observation':
-        for k, v in value.items():
-            print("*****k: ", k)
-            print("*****v: ", v)
-            print("***********")
-            if k == 'image':
-                image = Image.open(io.BytesIO(v))
-                image.save(f"image_{k}.jpg")
-    else:
-        print("=====value: ", value)
-    print("================================================")
+import pdb; pdb.set_trace()
+print(data.keys(), len(data['steps']))
+for idx, step in enumerate(data['steps']):
+    for key, value in step.items():
+        if key == 'observation':
+            print("================================================")
+            for k, v in value.items():
+                if k == 'image':
+                    image = Image.open(io.BytesIO(v))
+                    image.save(f"image_{idx}.jpg")
+                else:
+                    print("=====k: ", k)
+                    print("=====v: ", v)
+                    print("\n ")
