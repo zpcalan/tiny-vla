07454dfdc5bc6d8c4334dfd87e35c0183e337272
zpcalan <1303359419@qq.com>
2026-04-20 22:11:04 +0800

    Parse new dataset

--- RT-X/parse_robo_data.py
+++ RT-X/parse_robo_data.py
@@ -2,10 +2,14 @@ import pickle
 import io
 from PIL import Image
 with open('/data/k8s/zpc/tiny-vla/RT-X/sample_berkeley_gnm_cory_hall.data.pickle', 'rb') as f:
+# with open('/data/k8s/zpc/tiny-vla/RT-X/sample_000000087204.data.pickle', 'rb') as f:
     data = pickle.load(f)
 
+print("single data keys are", data.keys())
+print("step number is", len(data['steps']))
+print("keys of single step", data['steps'][0].keys())
+print("obs are", data['steps'][0]['observation'].keys())
 import pdb; pdb.set_trace()
-print(data.keys(), len(data['steps']))
 for idx, step in enumerate(data['steps']):
     for key, value in step.items():
         if key == 'observation':
@@ -13,7 +17,7 @@ for idx, step in enumerate(data['steps']):
             for k, v in value.items():
                 if k == 'image':
                     image = Image.open(io.BytesIO(v))
-                    image.save(f"image_{idx}.jpg")
+                    # image.save(f"image_{idx}.jpg")
                 else:
                     print("=====k: ", k)
                     print("=====v: ", v)
